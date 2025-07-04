0cf12235a42fa2efac233f100c1bcd4e386c1b56
Brad Nulsen <brad.nulsen@grantek.com>
2025-07-04 12:06:47 -0400

    adjusted sprite

--- .godot/mono/temp/obj/Debug/Shield Squire.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Shield Squire.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shield Squire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25319e09626e8133a8ca6bc40272ae1e1863e3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c069b64c49decd7f36a13cf45a237287ab0e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shield Squire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shield Squire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
